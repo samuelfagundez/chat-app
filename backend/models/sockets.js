c53311c9e0cfa18392e14396efc83732bfd785e1
Samuel <samuelfagundez97@gmail.com>
2021-06-03 20:11:39 -0400

    Sockets guide

--- backend/models/sockets.js
+++ backend/models/sockets.js
@@ -12,6 +12,16 @@ class Sockets {
     socketEvents() {
         // On connection
         this.io.on('connection', ( socket ) => {
+          // TODO: Validar jwt
+          // SI el JWT No es valido, desconectar
+          // TODO: Saber que el usuario esta activo mediante el UID
+          // TODO: Emitir todos los usuarios conectados
+          // TODO: Socket join, uid
+          // TODO: Escuchar cuando el cliente manda un mensaje
+          // mensaje personal
+          // TODO: Disconnect
+          // marcar en la base de datos que se desconecto
+          // TODO: Emitir todos los usuarios conectados
         });
     }
 
